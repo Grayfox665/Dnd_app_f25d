f9717c4aee9ab2e751f40071bd071185c3d6b095
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2026-03-19 16:21:47 +0100

    installed and added materialdesignicons for the tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,24 @@
 import { Tabs } from "expo-router";
+import MaterialDesignIcons from "@react-native-vector-icons/material-design-icons";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="Rules" options={{ title: "Rules" }} />
-      <Tabs.Screen name="Conditions" options={{ title: "Conditions" }} />
+    <Tabs 
+    screenOptions={{ tabBarActiveTintColor: "#6b100d", tabBarStyle: {
+      backgroundColor: "#ebd1b1"
+    }, headerShown : false }}>
+      <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: ({ color }) =>  (
+        <MaterialDesignIcons name="bookshelf" color={color} size={24} />
+      ) }} />
+      <Tabs.Screen name="Rules" options={{ title: "Rules", tabBarIcon: ({ color }) => (
+        <MaterialDesignIcons name="book-open-page-variant" color={color} size={24} />
+      ) }} />
+      <Tabs.Screen name="Conditions" options={{ title: "Conditions", tabBarIcon: ({ color }) => (
+        <MaterialDesignIcons name="chart-bubble" color={color} size={24} />
+      ) }} />
+      <Tabs.Screen name="Spells" options={{ title: "Spells", tabBarIcon: ({ color }) => (
+        <MaterialDesignIcons name="magic-staff" color={color} size={24} />
+      ) }} />
     </Tabs>
   )
 }
