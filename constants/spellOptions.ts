81072a91af1f40557b01c7dbda1c6073712b8933
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2026-03-20 10:41:37 +0100

    moved the spell options to it's own file in a folder constants

---
+++ constants/spellOptions.ts
@@ -0,0 +1,25 @@
+  export const schoolOfMagiOptions = [
+    { label: "All Schools", value: "all" },
+    { label: "Abjuration", value: "abjuration" },
+    { label: "Conjuration", value: "conjuration" },
+    { label: "Divination", value: "divination" },
+    { label: "Enchantment", value: "enchantment" },
+    { label: "Evocation", value: "evocation" },
+    { label: "Illusion", value: "illusion" },
+    { label: "Necromancy", value: "necromancy" },
+    { label: "Transmutation", value: "transmutation" },
+  ]
+
+  export const spellLevelOptions = [
+    { label : "All Levels", value: "all"},
+    { label : "Cantrip", value: "0"},
+    { label : "1", value: "1"},
+    { label : "2", value: "2"},
+    { label : "3", value: "3"},
+    { label : "4", value: "4"},
+    { label : "5", value: "5"},
+    { label : "6", value: "6"},
+    { label : "7", value: "7"},
+    { label : "8", value: "8"},
+    { label : "9", value: "9"},
+  ]
